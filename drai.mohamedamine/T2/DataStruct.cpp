8e09119f97241cbe7984c94449197b8bf9a1846c
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-05-07 11:46:19 +0200

    Update DataStruct.cpp

--- drai.mohamedamine/T2/DataStruct.cpp
+++ drai.mohamedamine/T2/DataStruct.cpp
@@ -1,19 +1,56 @@
-#ifndef DATASTRUCT_H
-#define DATASTRUCT_H
+#include "DataStruct.h"
+#include <regex>
+#include <iomanip>
 
-#include <string>
-#include <utility>
-#include <iostream>
-#include <sstream>
+bool isValidDataStruct(const std::string& line) {
+    return line.find("key1") != std::string::npos &&
+           line.find("key2") != std::string::npos &&
+           line.find("key3") != std::string::npos;
+}
 
-struct DataStruct {
-    double key1;
-    std::pair<long long, unsigned long long> key2;
-    std::string key3;
-};
+std::istream& operator>>(std::istream& in, DataStruct& data) {
+    std::string line;
+    std::getline(in, line);
 
-std::istream& operator>>(std::istream& in, DataStruct& data);
-std::ostream& operator<<(std::ostream& out, const DataStruct& data);
-bool isValidDataStruct(const std::string& line);
+    if (!isValidDataStruct(line)) {
+        in.setstate(std::ios::failbit);
+        return in;
+    }
 
-#endif // DATASTRUCT_H
+    try {
+        std::smatch match;
+
+        std::regex key1_regex(R"(key1\s+([-+]?[0-9]*\.?[0-9]+[eE][-+]?[0-9]+))");
+        std::regex key2_regex(R"(key2\s+\(:N\s+(-?\d+):D\s+(\d+):\))");
+        std::regex key3_regex(R"(key3\s+"([^"]*)")");
+
+        if (std::regex_search(line, match, key1_regex)) {
+            data.key1 = std::stod(match[1]);
+        } else {
+            throw std::invalid_argument("Invalid key1");
+        }
+
+        if (std::regex_search(line, match, key2_regex)) {
+            data.key2 = { std::stoll(match[1]), std::stoull(match[2]) };
+        } else {
+            throw std::invalid_argument("Invalid key2");
+        }
+
+        if (std::regex_search(line, match, key3_regex)) {
+            data.key3 = match[1];
+        } else {
+            throw std::invalid_argument("Invalid key3");
+        }
+    } catch (...) {
+        in.setstate(std::ios::failbit);
+    }
+
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const DataStruct& data) {
+    out << "(:key1 " << std::scientific << std::setprecision(1) << data.key1
+        << ":key2 (:N " << data.key2.first << ":D " << data.key2.second
+        << ":):key3 \"" << data.key3 << "\":)";
+    return out;
+}
