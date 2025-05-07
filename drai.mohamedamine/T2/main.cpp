8a0da245d6a72eeea2a8df66e9ea6dc4b499b85f
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-05-07 11:47:11 +0200

    Update main.cpp

--- drai.mohamedamine/T2/main.cpp
+++ drai.mohamedamine/T2/main.cpp
@@ -1,30 +1,27 @@
-#include <iostream>
+#include "DataStruct.h"
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include "DataStruct.h"
+#include <iostream>
 
-int main()
-{
-    using nspace::DataStruct;
+int main() {
     std::vector<DataStruct> data;
 
-    // Read from stdin (or file)
-    std::copy(
-        std::istream_iterator<DataStruct>(std::cin),
-        std::istream_iterator<DataStruct>(),
-        std::back_inserter(data)
-    );
+    std::istream_iterator<DataStruct> inputBegin(std::cin);
+    std::istream_iterator<DataStruct> inputEnd;
+    std::copy(inputBegin, inputEnd, std::back_inserter(data));
 
-    // Sort
-    std::sort(data.begin(), data.end(), nspace::compare);
+    std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
+        if (a.key1 != b.key1)
+            return a.key1 < b.key1;
+        if (a.key2 != b.key2)
+            return (static_cast<long double>(a.key2.first) / a.key2.second) <
+                   (static_cast<long double>(b.key2.first) / b.key2.second);
+        return a.key3.length() < b.key3.length();
+    });
 
-    // Output
-    std::copy(
-        data.begin(),
-        data.end(),
-        std::ostream_iterator<DataStruct>(std::cout, "\n")
-    );
+    std::ostream_iterator<DataStruct> output(std::cout, "\n");
+    std::copy(data.begin(), data.end(), output);
 
     return 0;
 }
