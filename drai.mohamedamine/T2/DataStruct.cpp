cc5cd4de487c1aaf92268adeb5f4559ee13d082a
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-05-07 11:45:35 +0200

    Update DataStruct.cpp

--- drai.mohamedamine/T2/DataStruct.cpp
+++ drai.mohamedamine/T2/DataStruct.cpp
@@ -1,118 +1,19 @@
-#include "DataStruct.h"
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
 
-namespace nspace
-{
-    // Parse ULL LIT (e.g., 89u11, 89ULL)
-    std::istream& operator>>(std::istream& in, UllLitIO&& dest)
-    {
-        std::istream::sentry sentry(in);
-        if (!sentry) return in;
-        
-        std::string input;
-        in >> input;
+#include <string>
+#include <utility>
+#include <iostream>
+#include <sstream>
 
-        // Remove 'u' or 'ULL' suffix (if present)
-        size_t suffix_pos = input.find_first_not_of("0123456789");
-        if (suffix_pos != std::string::npos)
-        {
-            input = input.substr(0, suffix_pos);
-        }
+struct DataStruct {
+    double key1;
+    std::pair<long long, unsigned long long> key2;
+    std::string key3;
+};
 
-        std::istringstream iss(input);
-        iss >> dest.ref;
-        return in;
-    }
+std::istream& operator>>(std::istream& in, DataStruct& data);
+std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+bool isValidDataStruct(const std::string& line);
 
-    // Parse ULL BIN (strictly 0b or 0B prefix)
-    std::istream& operator>>(std::istream& in, UllBinIO&& dest)
-    {
-        std::istream::sentry sentry(in);
-        if (!sentry) return in;
-
-        in >> std::ws;
-        if (in.peek() == '0')
-        {
-            in.get();
-            char next = in.peek();
-            if (next == 'b' || next == 'B')
-            {
-                in.get();
-                in >> std::setbase(2) >> dest.ref;
-            }
-            else
-            {
-                in.unget();
-                in.setstate(std::ios::failbit);
-            }
-        }
-        else
-        {
-            in.setstate(std::ios::failbit);
-        }
-        return in;
-    }
-
-    // Parse DataStruct
-    std::istream& operator>>(std::istream& in, DataStruct& dest)
-    {
-        std::istream::sentry sentry(in);
-        if (!sentry) return in;
-        int streamStartPos = in.tellg();
-        DataStruct input;
-        int keyNumber;
-
-        in >> DelimiterIO{ '(' };
-        for (int i = 0; i < 3; i++)
-        {
-            in >> DelimiterIO{ ':' };
-            in >> LabelIO{ "key" };
-            in >> KetNumIO{ keyNumber };
-            if (!in) break;
-
-            switch (keyNumber)
-            {
-            case 1:
-                in >> UllLitIO{ input.key1 };  // Read as ULL LIT
-                break;
-            case 2:
-                in >> UllBinIO{ input.key2 };  // Read as ULL BIN
-                break;
-            case 3:
-                in >> StringValueIO{ input.key3 };
-                break;
-            }
-        }
-        in >> DelimiterIO{ ':' } >> DelimiterIO{ ')' };
-
-        if (in) dest = input;
-        else
-        {
-            in.clear();
-            in.seekg(streamStartPos);
-            in.setstate(std::ios::failbit);
-        }
-        return in;
-    }
-
-    // Output DataStruct
-    std::ostream& operator<<(std::ostream& out, const DataStruct& src)
-    {
-        std::ostream::sentry sentry(out);
-        if (!sentry) return out;
-        iofmtguard fmtguard(out);
-        out << "(:key1 " << src.key1 << "u"  // ULL LIT format (e.g., 89u)
-            << ":key2 0b" << std::setbase(2) << src.key2  // ULL BIN format (e.g., 0b1010)
-            << ":key3 " << src.key3 << ":)";
-        return out;
-    }
-
-    // Comparison function (sort by key1, then key2, then key3 length)
-    bool compare(DataStruct a, DataStruct b)
-    {
-        if (a.key1 != b.key1) return a.key1 < b.key1;
-        if (a.key2 != b.key2) return a.key2 < b.key2;
-        return a.key3.length() < b.key3.length();
-    }
-
-    // (Rest of the operators remain the same)
-}
+#endif // DATASTRUCT_H
