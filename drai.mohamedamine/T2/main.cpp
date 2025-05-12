da8886a0f8afb806bbd261d561a13d563cc38515
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-05-13 01:09:45 +0200

    Update main.cpp

--- drai.mohamedamine/T2/main.cpp
+++ drai.mohamedamine/T2/main.cpp
@@ -10,11 +10,8 @@ int main() {
   using DSContainer = std::vector<DataStruct>;
   using InputIt = std::istream_iterator<DataStruct>;
   using OutIt = std::ostream_iterator<DataStruct>;
-
   try {
     DSContainer vals;
-    
-    // Read until EOF
     while (true) {
       std::copy(InputIt(std::cin), InputIt{}, std::back_inserter(vals));
       if (std::cin.eof()) break;
@@ -23,15 +20,12 @@ int main() {
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       }
     }
-
     if (vals.empty()) {
       std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
       return 0;
     }
-
     std::sort(vals.begin(), vals.end());
     std::copy(vals.begin(), vals.end(), OutIt(std::cout, "\n"));
-    
   } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << '\n';
     return 1;
