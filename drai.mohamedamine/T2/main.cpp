5f4e340f8f49260674c9007461a522ffa6f83c79
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-05-13 01:02:59 +0200

    Update main.cpp

--- drai.mohamedamine/T2/main.cpp
+++ drai.mohamedamine/T2/main.cpp
@@ -10,9 +10,11 @@ int main() {
   using DSContainer = std::vector<DataStruct>;
   using InputIt = std::istream_iterator<DataStruct>;
   using OutIt = std::ostream_iterator<DataStruct>;
+
   try {
     DSContainer vals;
-<<<<<<< HEAD
+    
+    // Read until EOF
     while (true) {
       std::copy(InputIt(std::cin), InputIt{}, std::back_inserter(vals));
       if (std::cin.eof()) break;
@@ -21,20 +23,15 @@ int main() {
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       }
     }
+
     if (vals.empty()) {
       std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
       return 0;
     }
-=======
-    while (true) {
-      std::copy(InputIt(std::cin), InputIt{}, std::back_inserter(vals));
-      if (std::cin.eof()) break;
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
->>>>>>> 2098b4e523bd3cd8b89cb3963e8eb32ccec3c7f7
+
     std::sort(vals.begin(), vals.end());
     std::copy(vals.begin(), vals.end(), OutIt(std::cout, "\n"));
+    
   } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << '\n';
     return 1;
