69f86f0415661286b06b0b04ed80dae1a315473f
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-05-13 05:39:47 +0200

    Update data.cpp

--- drai.mohamedamine/T2/data.cpp
+++ drai.mohamedamine/T2/data.cpp
@@ -4,33 +4,27 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-
 bool amine::operator<(const DataStruct &lhs, const DataStruct &rhs)
 {
     if (lhs.key1 != rhs.key1)
         return lhs.key1 < rhs.key1;
-    
     double lhs_ratio = static_cast<double>(lhs.key2.first) / lhs.key2.second;
     double rhs_ratio = static_cast<double>(rhs.key2.first) / rhs.key2.second;
     if (lhs_ratio != rhs_ratio)
         return lhs_ratio < rhs_ratio;
-    
     return lhs.key3.size() < rhs.key3.size();
 }
 std::ostream &amine::operator<<(std::ostream &out, const DataStruct &src)
 {
     std::ostream::sentry sentry(out);
     if (!sentry) return out;
-
     StreamGuard fg(out);
     out << std::scientific << std::setprecision(1);
-    
     out << "(:key1 " << src.key1
         << ":key2 (:N " << src.key2.first << ":D " << src.key2.second << ":)"
         << ":key3 \"" << src.key3 << "\":)";
     return out;
 }
-
 std::istream &amine::operator>>(std::istream &in, DataStruct &dest)
 {
     std::istream::sentry sentry(in);
@@ -62,6 +56,6 @@ std::istream &amine::operator>>(std::istream &in, DataStruct &dest)
     if (in && has_key1 && has_key2 && has_key3)
         dest = tmp;
     else
-        in.setstate(std::ios::failbit);   
+        in.setstate(std::ios::failbit);
     return in;
 }
