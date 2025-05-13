d85ed0c02f986dbdae0569ed78cde6c261c4e304
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-05-13 05:41:26 +0200

    Update test.cpp

--- drai.mohamedamine/T2/test.cpp
+++ drai.mohamedamine/T2/test.cpp
@@ -4,21 +4,17 @@
 #include <cctype>
 
 namespace amine {
-
 std::istream &operator>>(std::istream &in, DelimiterIO &&dest) {
     std::istream::sentry sentry(in);
     if (!sentry) return in;
-
     char c;
     if (in >> c && c != dest.exp)
         in.setstate(std::ios::failbit);
     return in;
 }
-
 std::istream &operator>>(std::istream &in, DoubleSCI &&dest) {
     std::istream::sentry sentry(in);
     if (!sentry) return in;
-
     double num;
     if (in >> num)
         dest.ref = num;
@@ -26,37 +22,30 @@ std::istream &operator>>(std::istream &in, DoubleSCI &&dest) {
         in.setstate(std::ios::failbit);
     return in;
 }
-
 std::istream &operator>>(std::istream &in, RationalIO &&dest) {
     std::istream::sentry sentry(in);
     if (!sentry) return in;
-
     long long n;
     unsigned long long d;
-    
     in >> DelimiterIO{'('} >> DelimiterIO{':'} >> DelimiterIO{'N'};
     in >> n;
     in >> DelimiterIO{':'} >> DelimiterIO{'D'};
     in >> d;
     in >> DelimiterIO{':'} >> DelimiterIO{')'};
-
     if (d == 0 || !in)
         in.setstate(std::ios::failbit);
     else
         dest.ref = {n, d};
     return in;
 }
-
 std::istream &operator>>(std::istream &in, StringIO &&dest) {
     std::istream::sentry sentry(in);
     if (!sentry) return in;
-
     char quote;
     if (in >> quote && quote != '"') {
         in.setstate(std::ios::failbit);
         return in;
     }
-
     std::string str;
     char c;
     while (in.get(c) {
@@ -71,12 +60,10 @@ std::istream &operator>>(std::istream &in, StringIO &&dest) {
             str += c;
         }
     }
-
     if (c != '"')
         in.setstate(std::ios::failbit);
     else
         dest.ref = str;
     return in;
 }
-
 }
