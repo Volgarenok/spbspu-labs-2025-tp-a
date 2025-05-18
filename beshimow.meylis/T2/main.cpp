a436458a9a3590a30d5f369de22a822882dbc139
Beshimov03 <meylisbesimow44@gmail.com>
2025-05-18 06:27:49 +0300

    for T2

--- beshimow.meylis/T2/main.cpp
+++ beshimow.meylis/T2/main.cpp
@@ -8,34 +8,45 @@ int main()
 {
   std::vector<DataStruct> dataVec;
 
-  std::istream_iterator<DataStruct> inputBegin(std::cin);
-  std::istream_iterator<DataStruct> inputEnd;
-  std::copy(inputBegin, inputEnd, std::back_inserter(dataVec));
-
-  std::sort(dataVec.begin(), dataVec.end(),
-    [](const DataStruct& a, const DataStruct& b)
+  try
+  {
+    std::istream_iterator<DataStruct> inputBegin(std::cin);
+    std::istream_iterator<DataStruct> inputEnd;
+    bool hasValidData = false;
+    for (auto it = inputBegin; it != inputEnd; ++it)
     {
-      if (a.key1.real() != b.key1.real())
-      {
-        return a.key1.real() < b.key1.real();
-      }
-      if (a.key1.imag() != b.key1.imag())
-      {
-        return a.key1.imag() < b.key1.imag();
-      }
-      if (a.key2.first != b.key2.first)
-      {
-        return a.key2.first < b.key2.first;
-      }
-      if (a.key2.second != b.key2.second)
-      {
-        return a.key2.second < b.key2.second;
-      }
-      return a.key3.size() < b.key3.size();
-    });
+      dataVec.push_back(*it);
+      hasValidData = true;
+    }
 
-  std::ostream_iterator<DataStruct> outIter(std::cout, "\n");
-  std::copy(dataVec.begin(), dataVec.end(), outIter);
+    if (!hasValidData)
+    {
+      std::cerr << "Atleast one supported record type\n";
+      return 1;
+    }
+
+    std::sort(dataVec.begin(), dataVec.end(),
+      [](const DataStruct& a, const DataStruct& b)
+      {
+        if (a.key1.real() != b.key1.real())
+          return a.key1.real() < b.key1.real();
+        if (a.key1.imag() != b.key1.imag())
+          return a.key1.imag() < b.key1.imag();
+        if (a.key2.first != b.key2.first)
+          return a.key2.first < b.key2.first;
+        if (a.key2.second != b.key2.second)
+          return a.key2.second < b.key2.second;
+        return a.key3.size() < b.key3.size();
+      });
+
+    std::ostream_iterator<DataStruct> outIter(std::cout, "\n");
+    std::copy(dataVec.begin(), dataVec.end(), outIter);
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
+    return 2;
+  }
 
   return 0;
 }
