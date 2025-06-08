9aed8860485738eefdc0be73feecf3a7fc271e5d
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-06-08 20:35:41 +0300

    added polygon.hpp

---
+++ zholobov.oleg/T3/polygon.hpp
@@ -0,0 +1,23 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include <iosfwd>
+#include <vector>
+
+namespace zholobov {
+
+  struct Point {
+    int x;
+    int y;
+  };
+
+  struct Polygon {
+    std::vector< Point > points;
+  };
+
+  std::istream& operator>>(std::istream& input, Point& point);
+  std::istream& operator>>(std::istream& input, Polygon& polygon);
+
+}
+
+#endif
