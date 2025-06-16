62db60a3755e3e2d080e67234b32eb44f02d8f7b
Robotophile <ksyunya.iv@gmail.com>
2025-06-16 18:50:43 +0300

    max

--- ivanova.ksenia/T3/commands.cpp
+++ ivanova.ksenia/T3/commands.cpp
@@ -54,4 +54,32 @@ namespace ivanova
       out << sum << '\n';
     }
   }
+
+  void max(std::istream& in, std::ostream& out, const std::vector<Polygon>& polygons)
+  {
+    if (polygons.empty())
+    {
+      throw std::invalid_argument("At least one polygon required");
+    }
+
+    std::string param;
+    in >> param;
+
+    if (param == "AREA")
+    {
+      auto maxIt = std::max_element(polygons.begin(), polygons.end(),
+              [](const Polygon& a, const Polygon& b){ return calculateArea(a) < calculateArea(b);});
+      out << std::fixed << std::setprecision(1) << calculateArea(*maxIt) << '\n';
+    }
+    else if (param == "VERTEXES")
+    {
+      auto maxIt = std::max_element(polygons.begin(), polygons.end(),
+              [](const Polygon& a, const Polygon& b) { return a.points.size() < b.points.size();});
+      out << maxIt->points.size() << '\n';
+    }
+    else
+    {
+      throw std::invalid_argument("Invalid max parameter");
+    }
+  }
 }
