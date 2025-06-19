1c17e91b443037040d38e44595ad8d0641811525
Robotophile <ksyunya.iv@gmail.com>
2025-06-20 00:08:47 +0300

    tea

--- ivanova.ksenia/T3/main.cpp
+++ ivanova.ksenia/T3/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char* argv[])
     {
       try
       {
-        if (command == "AREA") 
+        if (command == "AREA")
         {
           ivanova::area(std::cin, std::cout, polygons);
         }
