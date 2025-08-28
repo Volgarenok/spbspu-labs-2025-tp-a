60b132c838ff478927f552b1009d0466b504d7dd
Ste1mar <200306xstepx@gmail.com>
2025-08-28 18:28:25 +0300

    fixing input

--- lozinskii.stepan/T2/main.cpp
+++ lozinskii.stepan/T2/main.cpp
@@ -3,17 +3,27 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits>
 #include <iterator>
 
 int main()
 {
     std::vector<DataStruct> data;
 
-    std::copy(
+    while (!std::cin.eof())
+    {
+        std::copy(
         std::istream_iterator<DataStruct>(std::cin),
         std::istream_iterator<DataStruct>(),
         std::back_inserter(data)
     );
+
+    if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
 
     std::sort(data.begin(), data.end());
 
