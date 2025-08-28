1b88a98d00c3f459ce8f1c9f2cc015e7b4faf671
Ste1mar <200306xstepx@gmail.com>
2025-08-28 17:26:48 +0300

    fixing content issues season 2 episode 2

--- lozinskii.stepan/T2/DataStruct.hpp
+++ lozinskii.stepan/T2/DataStruct.hpp
@@ -5,9 +5,9 @@
 
 struct DataStruct
 {
-	long long key1_;
-	double key2_;
-	std::string key3_;
+    long long key1_;
+    double key2_;
+    std::string key3_;
 };
 
 bool operator<(const DataStruct& lhs, const DataStruct& rhs);
