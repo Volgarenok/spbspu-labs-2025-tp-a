05e4b692d1bb9d9fadf44bdbb1508c1c101970b3
Ksenia <f.kcusha082@mail.ru>
2025-09-20 00:39:31 +0300

    command add_word

---
+++ fedorova.ksenia/commands.cpp
@@ -0,0 +1,42 @@
+#include "commands.h"
+#include "dictionary.h"
+
+namespace
+{
+  using namespace fedorova;
+
+  WordSet::const_iterator findTranslate(const WordSet& list, const std::string& word)
+  {
+    return std::find(list.begin(), list.end(), word);
+  }
+}
+
+void fedorova::add_word(std::istream& is, std::ostream& os, DictSet& set)
+{
+  std::string word, translate, name;
+  is >> word >> translate >> name;
+  if (!is)
+  {
+    throw std::logic_error("<INVALID COMMAND>");
+  }
+
+  Dictionary& dict = set.at(name);
+  auto it_word = dict.find(word);
+  if (it_word == dict.end())
+  {
+    WordSet translates{ translate };
+    dict.emplace(word, translates);
+    os << "<WORD AND TRANSLATION HAVE ALREADY BEEN ADDED>";
+    return;
+  }
+
+  WordSet& translates = it_word->second;
+  if (findTranslate(translates, translate) == translates.cend())
+  {
+    translates.push_back(translate);
+    os << "<TRANSLATE WAS ADDED>";
+    return;
+  }
+
+  os << "<WORD AND TRANSLATE WERE ADDED>";
+}
