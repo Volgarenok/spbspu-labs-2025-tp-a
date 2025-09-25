1d165f69044fdbe28f61a74f7ee562dc6ead602d
guseynov.guseyn <ganx.gamer@gmail.com>
2025-09-25 22:43:02 +0300

    fix

--- guseynov.guseyn/T3/delimiter.cpp
+++ guseynov.guseyn/T3/delimiter.cpp
@@ -1,4 +1,5 @@
 #include "delimiter.hpp"
+#include <cctype>
 
 std::istream & guseynov::operator>>(std::istream & in, DelimiterI && dest)
 {
@@ -7,11 +8,21 @@ std::istream & guseynov::operator>>(std::istream & in, DelimiterI && dest)
   {
     return in;
   }
-  char c = in.get();
+  char c = 0;
+  in >> c;
   if (!in || c != dest.exp)
   {
     in.setstate(std::ios::failbit);
     return in;
+  }
+  if (dest.exp == ';')
+  {
+    char next = in.peek();
+    if (next == ')')
+    {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
   }
   return in;
 }
