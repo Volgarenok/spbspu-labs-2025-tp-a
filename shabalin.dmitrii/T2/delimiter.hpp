654c5d371573927c9a8f156d3a156876d05998c5
dmitei <dima9990860349@icloud.com>
2025-09-28 14:17:50 +0300

    hp

--- shabalin.dmitrii/T2/delimiter.hpp
+++ shabalin.dmitrii/T2/delimiter.hpp
@@ -1,4 +1,21 @@
-#ifndef DELIMITER_HPP
-#define DELIMITER_CPP
+#ifndef DELIMITER_H
+#define DELIMITER_H
 
+#include <iostream>
+
+namespace shabalin
+{
+  struct DelimiterSym
+  {
+    char expected;
+  };
+
+  struct DelimiterStr
+  {
+    const char* expected;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterSym&& exp);
+  std::istream& operator>>(std::istream& in, DelimiterStr&& exp);
+}
 #endif
