7ac75f2ff1963f309be421fced3364afedbe2cd2
dmitei <dima9990860349@icloud.com>
2025-09-28 14:21:11 +0300

    dwd

--- shabalin.dmitrii/T2/scopeGuard.hpp
+++ shabalin.dmitrii/T2/scopeGuard.hpp
@@ -1,4 +1,22 @@
-#ifndef SCOPEGUARD_HPP
-#define SCOPEGUARD_HPP
+#ifndef SCOPE_GUARD_HPP
+#define SCOPE_GUARD_HPP
 
+#include <iostream>
+
+namespace shabalin
+{
+  class ScopeGuard
+  {
+  public:
+    explicit ScopeGuard(std::basic_ios< char >&);
+
+    ~ScopeGuard();
+
+  private:
+    std::basic_ios< char >& s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
+}
 #endif
