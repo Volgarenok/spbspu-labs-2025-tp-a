f3e196289c05a0443315daad299d00ac1cd2f36f
dmitei <dima9990860349@icloud.com>
2025-09-28 16:46:39 +0300

    let me cry

--- shabalin.dmitrii/T2/keys.hpp
+++ shabalin.dmitrii/T2/keys.hpp
@@ -1,5 +1,30 @@
 #ifndef KEYS_HPP
 #define KEYS_HPP
 
+#include <complex>
+#include <istream>
+#include <string>
+
+namespace shabalin
+{
+  struct ComplexKey
+  {
+    std::complex< double >& value;
+  };
+  std::istream& operator>>(std::istream& in, ComplexKey&& key);
+
+  struct DoubleKey
+  {
+    double& value;
+  };
+
+  std::istream& operator>>(std::istream& in, DoubleKey&& key);
+
+  struct StringKey
+  {
+    std::string& value;
+  };
+  std::istream& operator>>(std::istream& in, StringKey&& key);
+}
 
 #endif
