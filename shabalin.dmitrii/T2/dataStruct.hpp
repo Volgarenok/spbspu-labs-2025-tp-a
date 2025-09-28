278d5127eb77fa1459e034783f8202c4e7edf15e
dmitei <dima9990860349@icloud.com>
2025-09-28 16:53:14 +0300

    moi budilnik shodit s uma

--- shabalin.dmitrii/T2/dataStruct.hpp
+++ shabalin.dmitrii/T2/dataStruct.hpp
@@ -1,5 +1,22 @@
-#ifndef DATASTRUCT_HPP
-#define DATASTRUCT_HPP
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+#include <complex>
+#include <istream>
+#include <ostream>
+#include <string>
+
+namespace shabalin
+{
+  struct DataStruct
+  {
+    std::complex< double > key1;
+    double key2;
+    std::string key3;
+    bool operator<(const DataStruct& other) const;
+  };
+  std::istream& operator>>(std::istream& in, DataStruct& value);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& value);
+}
 
 #endif
-
