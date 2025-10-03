24fa25d7ded1f70fc3c8eafee51f2bbb9ba7a0c6
uchuaip <d.smirnov2006@mail.ru>
2025-10-04 02:51:17 +0300

    const maps

--- smirnov.dmitriy/F0/main.cpp
+++ smirnov.dmitriy/F0/main.cpp
@@ -3,14 +3,13 @@
 #include <random>
 #include <limits>
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     if (argc < 2) {
         std::cerr << "Wrong arguments\n";
         return 1;
     }
 
-    setlocale(LC_ALL, "Russian");
     using namespace smirnov;
 
     dictionaries dictOfDicts;
@@ -21,42 +20,46 @@ int main(int argc, char **argv)
 
     using namespace std::placeholders;
 
-    std::map<std::string, std::function<void(dictionaries&, std::istream&)>> commandsIn;
-    std::map<std::string, std::function<void(dictionaries&, std::istream&, std::ostream&)>> commandsInOut;
+    const std::map<std::string, std::function<void(dictionaries&, std::istream&)>> commandsIn = {
+        {"createemptydict", createemptydict},
+        {"insertword", insertword},
+        {"insertfile", insertfile},
+        {"deleteword", deleteword},
+        {"clear", clear},
+        {"merge", merge},
+        {"intersect", intersect},
+        {"delete", deleteDict}
+    };
 
-    commandsIn["createemptydict"] = createemptydict;
-    commandsIn["insertword"] = insertword;
-    commandsIn["insertfile"] = insertfile;
-    commandsIn["deleteword"] = deleteword;
-    commandsIn["clear"] = clear;
-    commandsIn["merge"] = merge;
-    commandsIn["intersect"] = intersect;
-    commandsIn["delete"] = deleteDict;
-
-    commandsInOut["getfrequency"] = getfrequency;
-    commandsInOut["mostfrequent"] = mostfrequent;
-    commandsInOut["printsorted"] = printsorted;
-    commandsInOut["printbyfrequency"] = printbyfrequency;
-    commandsInOut["size"] = size;
-    commandsInOut["topprelativefrequent"] = topprelativefrequent;
-    commandsInOut["getrelativefrequency"] = getrelativefrequency;
-    commandsInOut["frequencyrangerelative"] = frequencyrangerelative;
-    commandsInOut["bottomprelativefrequent"] = bottomprelativefrequent;
-    commandsInOut["medianfrequency"] = medianfrequency;
+    const std::map<std::string, std::function<void(dictionaries&, std::istream&, std::ostream&)>> commandsInOut = {
+        {"getfrequency", getfrequency},
+        {"mostfrequent", mostfrequent},
+        {"printsorted", printsorted},
+        {"printbyfrequency", printbyfrequency},
+        {"size", size},
+        {"topprelativefrequent", topprelativefrequent},
+        {"getrelativefrequency", getrelativefrequency},
+        {"frequencyrangerelative", frequencyrangerelative},
+        {"bottomprelativefrequent", bottomprelativefrequent},
+        {"medianfrequency", medianfrequency}
+    };
 
     std::string cmd;
     while (std::cin >> cmd) {
         try {
             commandsIn.at(cmd)(dictOfDicts, std::cin);
-        } catch (const std::out_of_range &) {
+        }
+        catch (const std::out_of_range&) {
             try {
                 commandsInOut.at(cmd)(dictOfDicts, std::cin, std::cout);
-            } catch (const std::exception &e) {
+            }
+            catch (const std::exception& e) {
                 std::cerr << e.what() << '\n';
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             }
-        } catch (const std::exception &e) {
+        }
+        catch (const std::exception& e) {
             std::cerr << e.what() << '\n';
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
