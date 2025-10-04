d798b91a8c556dcd7c3f12c9ff64da0d3c9a6f5d
uchuaip <d.smirnov2006@mail.ru>
2025-10-04 03:33:35 +0300

    dictionaryhpp fix

--- smirnov.dmitriy/F0/dictionary.hpp
+++ smirnov.dmitriy/F0/dictionary.hpp
@@ -1,39 +1,37 @@
 #ifndef DICTIONARY_HPP
 #define DICTIONARY_HPP
 
+#include <iostream>
 #include <map>
-#include <string>
 #include <vector>
 
 namespace smirnov {
 
-  class Dictionary {
-  public:
-    Dictionary() = default;
+    class Dictionary {
+    public:
+        void insertWord(const std::string& word);
+        void insertFromFile(const std::string& filename);
+        bool contains(const std::string& word) const;
+        int getFrequency(const std::string& word) const;
+        std::string mostFrequent() const;
+        void deleteWord(const std::string& word);
+        void clear();
+        bool empty() const;
+        size_t size() const;
+        std::vector<std::pair<std::string, int>> getSortedByWord() const;
+        std::vector<std::pair<std::string, int>> getSortedByFrequency() const;
+        double getRelativeFrequency(const std::string& word) const;
+        std::vector<std::pair<std::string, double>> getTopRelative(int N) const;
+        std::vector<std::pair<std::string, double>> getBottomRelative(int N) const;
+        std::vector<std::pair<std::string, double>> getRangeRelative(double min, double max) const;
+        std::vector<std::string> medianFrequency() const;
 
-    void insertWord(const std::string& word);
-    void insertFromFile(const std::string& filename);
-    bool contains(const std::string& word) const;
-    int getFrequency(const std::string& word) const;
-    std::string mostFrequent() const;
-    void deleteWord(const std::string& word);
-    void clear();
-    bool empty() const;
-    size_t size() const;
+    private:
+        std::map<std::string, int> data;
+    };
 
-    std::vector<std::pair<std::string,int>> getSortedByWord() const;
-    std::vector<std::pair<std::string,int>> getSortedByFrequency() const;
-
-    double getRelativeFrequency(const std::string& word) const;
-    std::vector<std::pair<std::string,double>> getTopRelative(int N) const;
-    std::vector<std::pair<std::string,double>> getBottomRelative(int N) const;
-    std::vector<std::pair<std::string,double>> getRangeRelative(double min, double max) const;
-
-    std::vector<std::string> medianFrequency() const;
-
-  private:
-    std::map<std::string,int> data;
-  };
+    bool compareFrequency(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b);
+    bool compareByFrequency(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b);
 
 }
 
