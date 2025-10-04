7dc2e3fca0f356ae710547bea8961ea3cb114004
uchuaip <d.smirnov2006@mail.ru>
2025-10-04 03:48:06 +0300

    cg main.cpp

--- smirnov.dmitriy/F0/main.cpp
+++ smirnov.dmitriy/F0/main.cpp
@@ -5,65 +5,73 @@
 
 int main(int argc, char** argv)
 {
-    if (argc < 2) {
-        std::cerr << "Wrong arguments\n";
-        return 1;
+  if (argc < 2)
+  {
+    std::cerr << "Wrong arguments\n";
+    return 1;
+  }
+
+  using namespace smirnov;
+
+  dictionaries dictOfDicts;
+  std::string inputCmd(argv[1]);
+  if (inputCmd == "--help")
+  {
+    printHelp(std::cout);
+  }
+
+  using namespace std::placeholders;
+
+  const std::map<std::string, std::function<void(dictionaries&, std::istream&)>> commandsIn = {
+    {"createemptydict", createemptydict},
+    {"insertword", insertword},
+    {"insertfile", insertfile},
+    {"deleteword", deleteword},
+    {"clear", clear},
+    {"merge", merge},
+    {"intersect", intersect},
+    {"delete", deleteDict}
+  };
+
+  const std::map<std::string, std::function<void(dictionaries&, std::istream&, std::ostream&)>> commandsInOut = {
+    {"getfrequency", getfrequency},
+    {"mostfrequent", mostfrequent},
+    {"printsorted", printsorted},
+    {"printbyfrequency", printbyfrequency},
+    {"size", size},
+    {"topprelativefrequent", topprelativefrequent},
+    {"getrelativefrequency", getrelativefrequency},
+    {"frequencyrangerelative", frequencyrangerelative},
+    {"bottomprelativefrequent", bottomprelativefrequent},
+    {"medianfrequency", medianfrequency}
+  };
+
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      commandsIn.at(cmd)(dictOfDicts, std::cin);
     }
-
-    using namespace smirnov;
-
-    dictionaries dictOfDicts;
-    std::string inputCmd(argv[1]);
-    if (inputCmd == "--help") {
-        printHelp(std::cout);
+    catch (const std::out_of_range&)
+    {
+      try
+      {
+        commandsInOut.at(cmd)(dictOfDicts, std::cin, std::cout);
+      }
+      catch (const std::exception& e)
+      {
+        std::cerr << e.what() << '\n';
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      }
     }
-
-    using namespace std::placeholders;
-
-    const std::map<std::string, std::function<void(dictionaries&, std::istream&)>> commandsIn = {
-        {"createemptydict", createemptydict},
-        {"insertword", insertword},
-        {"insertfile", insertfile},
-        {"deleteword", deleteword},
-        {"clear", clear},
-        {"merge", merge},
-        {"intersect", intersect},
-        {"delete", deleteDict}
-    };
-
-    const std::map<std::string, std::function<void(dictionaries&, std::istream&, std::ostream&)>> commandsInOut = {
-        {"getfrequency", getfrequency},
-        {"mostfrequent", mostfrequent},
-        {"printsorted", printsorted},
-        {"printbyfrequency", printbyfrequency},
-        {"size", size},
-        {"topprelativefrequent", topprelativefrequent},
-        {"getrelativefrequency", getrelativefrequency},
-        {"frequencyrangerelative", frequencyrangerelative},
-        {"bottomprelativefrequent", bottomprelativefrequent},
-        {"medianfrequency", medianfrequency}
-    };
-
-    std::string cmd;
-    while (std::cin >> cmd) {
-        try {
-            commandsIn.at(cmd)(dictOfDicts, std::cin);
-        }
-        catch (const std::out_of_range&) {
-            try {
-                commandsInOut.at(cmd)(dictOfDicts, std::cin, std::cout);
-            }
-            catch (const std::exception& e) {
-                std::cerr << e.what() << '\n';
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            }
-        }
-        catch (const std::exception& e) {
-            std::cerr << e.what() << '\n';
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
+    catch (const std::exception& e)
+    {
+      std::cerr << e.what() << '\n';
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
+  }
 }
 
