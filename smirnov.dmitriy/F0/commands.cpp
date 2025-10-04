7e5423b1258db899c77bf91d7eeadc1a0b18284f
uchuaip <d.smirnov2006@mail.ru>
2025-10-04 03:47:24 +0300

    cg commands.cpp

--- smirnov.dmitriy/F0/commands.cpp
+++ smirnov.dmitriy/F0/commands.cpp
@@ -4,303 +4,361 @@
 #include <algorithm>
 #include <iomanip>
 
-void smirnov::printHelp(std::ostream& out) {
-    out << "Help for commands:\n";
-    out << "1. createemptydict <dictname>\n";
-    out << "2. insertword <dictname> <word>\n";
-    out << "3. insertfile <dictname> <filename>\n";
-    out << "4. getfrequency <dictname> <word>\n";
-    out << "5. mostfrequent <dictname>\n";
-    out << "6. deleteword <dictname> <word>\n";
-    out << "7. clear <dictname>\n";
-    out << "8. merge <dict1> <dict2> <newdict>\n";
-    out << "9. intersect <dict1> <dict2> <newdict>\n";
-    out << "10. printsorted <dictname>\n";
-    out << "11. printbyfrequency <dictname>\n";
-    out << "12. size <dictname>\n";
-    out << "13. topprelativefrequent <dictname> <N>\n";
-    out << "14. getrelativefrequency <dictname> <word>\n";
-    out << "15. frequencyrangerelative <dictname> <min> <max>\n";
-    out << "16. bottomprelativefrequent <dictname> <N>\n";
-    out << "17. medianfrequency <dictname>\n";
-    out << "18. delete <dictname>\n";
+void smirnov::printHelp(std::ostream& out)
+{
+  out << "Help for commands:\n";
+  out << "1. createemptydict <dictname>\n";
+  out << "2. insertword <dictname> <word>\n";
+  out << "3. insertfile <dictname> <filename>\n";
+  out << "4. getfrequency <dictname> <word>\n";
+  out << "5. mostfrequent <dictname>\n";
+  out << "6. deleteword <dictname> <word>\n";
+  out << "7. clear <dictname>\n";
+  out << "8. merge <dict1> <dict2> <newdict>\n";
+  out << "9. intersect <dict1> <dict2> <newdict>\n";
+  out << "10. printsorted <dictname>\n";
+  out << "11. printbyfrequency <dictname>\n";
+  out << "12. size <dictname>\n";
+  out << "13. topprelativefrequent <dictname> <N>\n";
+  out << "14. getrelativefrequency <dictname> <word>\n";
+  out << "15. frequencyrangerelative <dictname> <min> <max>\n";
+  out << "16. bottomprelativefrequent <dictname> <N>\n";
+  out << "17. medianfrequency <dictname>\n";
+  out << "18. delete <dictname>\n";
 }
 
-
-void smirnov::createemptydict(dictionaries& dicts, std::istream& in) {
-    std::string name;
-    in >> name;
-    if (dicts.find(name) != dicts.end()) {
-        std::cout << "<INVALID COMMAND>\n";
-        return;
-    }
-    dicts[name] = Dictionary();
+void smirnov::createemptydict(dictionaries& dicts, std::istream& in)
+{
+  std::string name;
+  in >> name;
+  if (dicts.find(name) != dicts.end())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+  dicts[name] = Dictionary();
 }
 
-void smirnov::insertword(dictionaries& dicts, std::istream& in) {
-    std::string dictname, word;
-    in >> dictname >> word;
-    if (dicts.find(dictname) == dicts.end()) {
-        std::cout << "<INVALID COMMAND>\n";
-        return;
-    }
-    dicts[dictname].insertWord(word);
+void smirnov::insertword(dictionaries& dicts, std::istream& in)
+{
+  std::string dictname, word;
+  in >> dictname >> word;
+  if (dicts.find(dictname) == dicts.end())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+  dicts[dictname].insertWord(word);
 }
 
-void smirnov::insertfile(dictionaries& dicts, std::istream& in) {
-    std::string dictname, filename;
-    in >> dictname >> filename;
-    if (dicts.find(dictname) == dicts.cend()) {
-        std::cout << "<INVALID COMMAND>\n";
-        return;
-    }
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cout << "<INVALID COMMAND>\n";
-        return;
-    }
-    dicts[dictname].insertFromFile(filename);
+void smirnov::insertfile(dictionaries& dicts, std::istream& in)
+{
+  std::string dictname, filename;
+  in >> dictname >> filename;
+  if (dicts.find(dictname) == dicts.cend())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+  std::ifstream file(filename);
+  if (!file.is_open())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+  dicts[dictname].insertFromFile(filename);
 }
 
-void smirnov::deleteword(dictionaries& dicts, std::istream& in) {
-    std::string dictname, word;
-    in >> dictname >> word;
-    if (dicts.find(dictname) == dicts.end() || !dicts[dictname].contains(word)) {
-        std::cout << "<INVALID COMMAND>\n";
-        return;
-    }
-    dicts[dictname].deleteWord(word);
+void smirnov::deleteword(dictionaries& dicts, std::istream& in)
+{
+  std::string dictname, word;
+  in >> dictname >> word;
+  if (dicts.find(dictname) == dicts.end() || !dicts[dictname].contains(word))
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+  dicts[dictname].deleteWord(word);
 }
 
-void smirnov::clear(dictionaries& dicts, std::istream& in) {
-    std::string dictname;
-    in >> dictname;
-    if (dicts.find(dictname) == dicts.end()) {
-        std::cout << "<INVALID COMMAND>\n";
-        return;
-    }
-    if (dicts[dictname].empty()) {
-        std::cout << "<ALREADY EMPTY>\n";
-        return;
-    }
-    dicts[dictname].clear();
+void smirnov::clear(dictionaries& dicts, std::istream& in)
+{
+  std::string dictname;
+  in >> dictname;
+  if (dicts.find(dictname) == dicts.end())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+  if (dicts[dictname].empty())
+  {
+    std::cout << "<ALREADY EMPTY>\n";
+    return;
+  }
+  dicts[dictname].clear();
 }
 
-void smirnov::merge(dictionaries& dicts, std::istream& in) {
-    std::string dict1, dict2, newdict;
-    in >> dict1 >> dict2 >> newdict;
+void smirnov::merge(dictionaries& dicts, std::istream& in)
+{
+  std::string dict1, dict2, newdict;
+  in >> dict1 >> dict2 >> newdict;
 
-    if (!in || dicts.find(dict1) == dicts.end() || dicts.find(dict2) == dicts.end() || dicts.find(newdict) != dicts.end()) {
-        std::cout << "<INVALID COMMAND>\n";
-        return;
-    }
+  if (!in || dicts.find(dict1) == dicts.end() || dicts.find(dict2) == dicts.end() || dicts.find(newdict) != dicts.end())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
 
-    const Dictionary& d1 = dicts[dict1];
-    const Dictionary& d2 = dicts[dict2];
+  const Dictionary& d1 = dicts[dict1];
+  const Dictionary& d2 = dicts[dict2];
 
-    if (d1.empty() && d2.empty()) {
-        std::cout << "<EMPTY>\n";
-        return;
-    }
+  if (d1.empty() && d2.empty())
+  {
+    std::cout << "<EMPTY>\n";
+    return;
+  }
 
-    Dictionary result;
+  Dictionary result;
 
-    for (auto& p : d1.getSortedByWord()) {
-        for (int i = 0; i < p.second; ++i)
-            result.insertWord(p.first);
-    }
+  for (auto& p : d1.getSortedByWord())
+  {
+    for (int i = 0; i < p.second; ++i)
+      result.insertWord(p.first);
+  }
 
-    for (auto& p : d2.getSortedByWord()) {
-        for (int i = 0; i < p.second; ++i)
-            result.insertWord(p.first);
-    }
+  for (auto& p : d2.getSortedByWord())
+  {
+    for (int i = 0; i < p.second; ++i)
+      result.insertWord(p.first);
+  }
 
-    dicts[newdict] = result;
+  dicts[newdict] = result;
 }
 
-void smirnov::intersect(dictionaries& dicts, std::istream& in) {
-    std::string dict1, dict2, newdict;
-    in >> dict1 >> dict2 >> newdict;
+void smirnov::intersect(dictionaries& dicts, std::istream& in)
+{
+  std::string dict1, dict2, newdict;
+  in >> dict1 >> dict2 >> newdict;
 
-    if (!in || dicts.find(dict1) == dicts.end() || dicts.find(dict2) == dicts.end() || dicts.find(newdict) != dicts.end()) {
-        std::cout << "<INVALID COMMAND>\n";
-        return;
+  if (!in || dicts.find(dict1) == dicts.end() || dicts.find(dict2) == dicts.end() || dicts.find(newdict) != dicts.end())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  const Dictionary& d1 = dicts[dict1];
+  const Dictionary& d2 = dicts[dict2];
+
+  Dictionary result;
+  for (auto& p : d1.getSortedByWord())
+  {
+    if (d2.contains(p.first))
+    {
+      int freq = p.second + d2.getFrequency(p.first);
+      for (int i = 0; i < freq; ++i)
+        result.insertWord(p.first);
     }
+  }
 
-    const Dictionary& d1 = dicts[dict1];
-    const Dictionary& d2 = dicts[dict2];
+  if (result.empty())
+  {
+    std::cout << "<EMPTY>\n";
+    return;
+  }
 
-    Dictionary result;
-    for (auto& p : d1.getSortedByWord()) {
-        if (d2.contains(p.first)) {
-            int freq = p.second + d2.getFrequency(p.first);
-            for (int i = 0; i < freq; ++i)
-                result.insertWord(p.first);
-        }
-    }
-
-    if (result.empty()) {
-        std::cout << "<EMPTY>\n";
-        return;
-    }
-
-    dicts[newdict] = result;
+  dicts[newdict] = result;
 }
 
-void smirnov::deleteDict(dictionaries& dicts, std::istream& in) {
-    std::string dictname;
-    in >> dictname;
+void smirnov::deleteDict(dictionaries& dicts, std::istream& in)
+{
+  std::string dictname;
+  in >> dictname;
 
-    if (!in || dicts.find(dictname) == dicts.end()) {
-        std::cout << "<INVALID COMMAND>\n";
-        return;
-    }
+  if (!in || dicts.find(dictname) == dicts.end())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
 
-    dicts.erase(dictname);
+  dicts.erase(dictname);
 }
 
-void smirnov::getfrequency(dictionaries& dicts, std::istream& in, std::ostream& out) {
-    std::string dictname, word;
-    in >> dictname >> word;
-    if (dicts.find(dictname) == dicts.end()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    int f = dicts[dictname].getFrequency(word);
-    if (f < 0) out << "<INVALID COMMAND>\n";
-    else out << f << "\n";
+void smirnov::getfrequency(dictionaries& dicts, std::istream& in, std::ostream& out)
+{
+  std::string dictname, word;
+  in >> dictname >> word;
+  if (dicts.find(dictname) == dicts.end())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  int f = dicts[dictname].getFrequency(word);
+  if (f < 0)
+    out << "<INVALID COMMAND>\n";
+  else
+    out << f << "\n";
 }
 
-void smirnov::mostfrequent(dictionaries& dicts, std::istream& in, std::ostream& out) {
-    std::string dictname;
-    in >> dictname;
-    if (dicts.find(dictname) == dicts.end() || dicts[dictname].empty()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    out << dicts[dictname].mostFrequent() << "\n";
+void smirnov::mostfrequent(dictionaries& dicts, std::istream& in, std::ostream& out)
+{
+  std::string dictname;
+  in >> dictname;
+  if (dicts.find(dictname) == dicts.end() || dicts[dictname].empty())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  out << dicts[dictname].mostFrequent() << "\n";
 }
 
-void smirnov::printsorted(dictionaries& dicts, std::istream& in, std::ostream& out) {
-    std::string dictname;
-    in >> dictname;
-    if (!in || dicts.find(dictname) == dicts.end()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    for (auto& p : dicts[dictname].getSortedByWord()) {
-        out << p.first << ": " << p.second << "\n";
-    }
+void smirnov::printsorted(dictionaries& dicts, std::istream& in, std::ostream& out)
+{
+  std::string dictname;
+  in >> dictname;
+  if (!in || dicts.find(dictname) == dicts.end())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  for (auto& p : dicts[dictname].getSortedByWord())
+  {
+    out << p.first << ": " << p.second << "\n";
+  }
 }
 
-void smirnov::printbyfrequency(dictionaries& dicts, std::istream& in, std::ostream& out) {
-    std::string dictname;
-    in >> dictname;
-    if (!in || dicts.find(dictname) == dicts.end()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    for (auto& p : dicts[dictname].getSortedByFrequency()) {
-        out << p.first << ": " << p.second << "\n";
-    }
+void smirnov::printbyfrequency(dictionaries& dicts, std::istream& in, std::ostream& out)
+{
+  std::string dictname;
+  in >> dictname;
+  if (!in || dicts.find(dictname) == dicts.end())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  for (auto& p : dicts[dictname].getSortedByFrequency())
+  {
+    out << p.first << ": " << p.second << "\n";
+  }
 }
 
-void smirnov::size(dictionaries& dicts, std::istream& in, std::ostream& out) {
-    std::string dictname;
-    in >> dictname;
-    if (!in || dicts.find(dictname) == dicts.end()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    out << dicts[dictname].size() << "\n";
+void smirnov::size(dictionaries& dicts, std::istream& in, std::ostream& out)
+{
+  std::string dictname;
+  in >> dictname;
+  if (!in || dicts.find(dictname) == dicts.end())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  out << dicts[dictname].size() << "\n";
 }
 
-void smirnov::topprelativefrequent(dictionaries& dicts, std::istream& in, std::ostream& out) {
-    std::string dictname;
-    int N;
-    in >> dictname >> N;
-    if (!in || dicts.find(dictname) == dicts.end()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    auto result = dicts[dictname].getTopRelative(N);
-    if (result.empty()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    out << std::fixed << std::setprecision(6);
-    for (auto& p : result) {
-        out << p.first << ": " << p.second << "\n";
-    }
+void smirnov::topprelativefrequent(dictionaries& dicts, std::istream& in, std::ostream& out)
+{
+  std::string dictname;
+  int N;
+  in >> dictname >> N;
+  if (!in || dicts.find(dictname) == dicts.end())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  auto result = dicts[dictname].getTopRelative(N);
+  if (result.empty())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  out << std::fixed << std::setprecision(6);
+  for (auto& p : result)
+  {
+    out << p.first << ": " << p.second << "\n";
+  }
 }
 
-void smirnov::getrelativefrequency(dictionaries& dicts, std::istream& in, std::ostream& out) {
-    std::string dictname, word;
-    in >> dictname >> word;
-    if (!in || dicts.find(dictname) == dicts.end()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    double freq = dicts[dictname].getRelativeFrequency(word);
-    if (freq < 0) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    out << std::fixed << std::setprecision(6);
-    out << word << ": " << freq << "\n";
+void smirnov::getrelativefrequency(dictionaries& dicts, std::istream& in, std::ostream& out)
+{
+  std::string dictname, word;
+  in >> dictname >> word;
+  if (!in || dicts.find(dictname) == dicts.end())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  double freq = dicts[dictname].getRelativeFrequency(word);
+  if (freq < 0)
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  out << std::fixed << std::setprecision(6);
+  out << word << ": " << freq << "\n";
 }
 
-void smirnov::frequencyrangerelative(dictionaries& dicts, std::istream& in, std::ostream& out) {
-    std::string dictname;
-    double min, max;
-    in >> dictname >> min >> max;
-    if (!in || dicts.find(dictname) == dicts.end() || min > max) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    auto result = dicts[dictname].getRangeRelative(min, max);
-    if (result.empty()) {
-        out << "<EMPTY>\n";
-        return;
-    }
-    out << std::fixed << std::setprecision(6);
-    for (auto& p : result) {
-        out << p.first << ": " << p.second << "\n";
-    }
+void smirnov::frequencyrangerelative(dictionaries& dicts, std::istream& in, std::ostream& out)
+{
+  std::string dictname;
+  double min, max;
+  in >> dictname >> min >> max;
+  if (!in || dicts.find(dictname) == dicts.end() || min > max)
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  auto result = dicts[dictname].getRangeRelative(min, max);
+  if (result.empty())
+  {
+    out << "<EMPTY>\n";
+    return;
+  }
+  out << std::fixed << std::setprecision(6);
+  for (auto& p : result)
+  {
+    out << p.first << ": " << p.second << "\n";
+  }
 }
 
-void smirnov::bottomprelativefrequent(dictionaries& dicts, std::istream& in, std::ostream& out) {
-    std::string dictname;
-    int N;
-    in >> dictname >> N;
-    if (!in || dicts.find(dictname) == dicts.end()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    auto result = dicts[dictname].getBottomRelative(N);
-    if (result.empty()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    out << std::fixed << std::setprecision(6);
-    for (auto& p : result) {
-        out << p.first << ": " << p.second << "\n";
-    }
+void smirnov::bottomprelativefrequent(dictionaries& dicts, std::istream& in, std::ostream& out)
+{
+  std::string dictname;
+  int N;
+  in >> dictname >> N;
+  if (!in || dicts.find(dictname) == dicts.end())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  auto result = dicts[dictname].getBottomRelative(N);
+  if (result.empty())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  out << std::fixed << std::setprecision(6);
+  for (auto& p : result)
+  {
+    out << p.first << ": " << p.second << "\n";
+  }
 }
 
-void smirnov::medianfrequency(dictionaries& dicts, std::istream& in, std::ostream& out) {
-    std::string dictname;
-    in >> dictname;
-    if (!in || dicts.find(dictname) == dicts.end()) {
-        out << "<INVALID COMMAND>\n";
-        return;
-    }
-    auto result = dicts[dictname].medianFrequency();
-    if (dicts[dictname].empty()) {
-        out << "<EMPTY>\n";
-        return;
-    }
-    for (auto& w : result) {
-        out << w << "\n";
-    }
+void smirnov::medianfrequency(dictionaries& dicts, std::istream& in, std::ostream& out)
+{
+  std::string dictname;
+  in >> dictname;
+  if (!in || dicts.find(dictname) == dicts.end())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  auto result = dicts[dictname].medianFrequency();
+  if (dicts[dictname].empty())
+  {
+    out << "<EMPTY>\n";
+    return;
+  }
+  for (auto& w : result)
+  {
+    out << w << "\n";
+  }
 }
+
