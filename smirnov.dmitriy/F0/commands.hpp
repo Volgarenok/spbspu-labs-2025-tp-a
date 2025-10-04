87fd71cc0af4e0b55fcffed4c8b3d6e929f29719
uchuaip <d.smirnov2006@mail.ru>
2025-10-04 03:46:05 +0300

    cg commands.hpp

--- smirnov.dmitriy/F0/commands.hpp
+++ smirnov.dmitriy/F0/commands.hpp
@@ -6,32 +6,32 @@
 #include <string>
 #include <iostream>
 
-namespace smirnov {
+namespace smirnov
+{
+  using dictionaries = std::map<std::string, Dictionary>;
 
-    using dictionaries = std::map<std::string, Dictionary>;
-    void printHelp(std::ostream& out);
-    void createemptydict(dictionaries& dicts, std::istream& in);
-    void insertword(dictionaries& dicts, std::istream& in);
-    void insertfile(dictionaries& dicts, std::istream& in);
-    void deleteword(dictionaries& dicts, std::istream& in);
-    void clear(dictionaries& dicts, std::istream& in);
-    void merge(dictionaries& dicts, std::istream& in);
-    void intersect(dictionaries& dicts, std::istream& in);
-    void deleteDict(dictionaries& dicts, std::istream& in);
+  void printHelp(std::ostream& out);
+  void createemptydict(dictionaries& dicts, std::istream& in);
+  void insertword(dictionaries& dicts, std::istream& in);
+  void insertfile(dictionaries& dicts, std::istream& in);
+  void deleteword(dictionaries& dicts, std::istream& in);
+  void clear(dictionaries& dicts, std::istream& in);
+  void merge(dictionaries& dicts, std::istream& in);
+  void intersect(dictionaries& dicts, std::istream& in);
+  void deleteDict(dictionaries& dicts, std::istream& in);
 
-    void getfrequency(dictionaries& dicts, std::istream& in, std::ostream& out);
-    void mostfrequent(dictionaries& dicts, std::istream& in, std::ostream& out);
-    void printsorted(dictionaries& dicts, std::istream& in, std::ostream& out);
-    void printbyfrequency(dictionaries& dicts, std::istream& in, std::ostream& out);
-    void size(dictionaries& dicts, std::istream& in, std::ostream& out);
-    void topprelativefrequent(dictionaries& dicts, std::istream& in, std::ostream& out);
-    void getrelativefrequency(dictionaries& dicts, std::istream& in, std::ostream& out);
-    void frequencyrangerelative(dictionaries& dicts, std::istream& in, std::ostream& out);
-    void bottomprelativefrequent(dictionaries& dicts, std::istream& in, std::ostream& out);
-    void medianfrequency(dictionaries& dicts, std::istream& in, std::ostream& out);
-
-
+  void getfrequency(dictionaries& dicts, std::istream& in, std::ostream& out);
+  void mostfrequent(dictionaries& dicts, std::istream& in, std::ostream& out);
+  void printsorted(dictionaries& dicts, std::istream& in, std::ostream& out);
+  void printbyfrequency(dictionaries& dicts, std::istream& in, std::ostream& out);
+  void size(dictionaries& dicts, std::istream& in, std::ostream& out);
+  void topprelativefrequent(dictionaries& dicts, std::istream& in, std::ostream& out);
+  void getrelativefrequency(dictionaries& dicts, std::istream& in, std::ostream& out);
+  void frequencyrangerelative(dictionaries& dicts, std::istream& in, std::ostream& out);
+  void bottomprelativefrequent(dictionaries& dicts, std::istream& in, std::ostream& out);
+  void medianfrequency(dictionaries& dicts, std::istream& in, std::ostream& out);
 
 }
 
 #endif
+
