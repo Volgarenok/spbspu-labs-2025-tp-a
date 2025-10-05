7a55e3cab2537af262afe606986ae44f51b14eef
uchuaip <d.smirnov2006@mail.ru>
2025-10-05 12:06:32 +0300

    add std::bind

--- smirnov.dmitriy/F0/main.cpp
+++ smirnov.dmitriy/F0/main.cpp
@@ -3,31 +3,6 @@
 #include <map>
 #include "commands.hpp"
 
-namespace smirnov
-{
-  struct CommandWrapper
-  {
-    std::function< void(dictionaries&, std::istream&) > inOnly;
-    std::function< void(dictionaries&, std::istream&, std::ostream&) > inOut;
-
-    void operator()(dictionaries& dicts, std::istream& in, std::ostream& out) const
-    {
-      if (inOnly)
-      {
-        inOnly(dicts, in);
-      }
-      else if (inOut)
-      {
-        inOut(dicts, in, out);
-      }
-      else
-      {
-        throw std::invalid_argument("<INVALID COMMAND>");
-      }
-    }
-  };
-}
-
 int main(int argc, char** argv)
 {
   using namespace smirnov;
@@ -46,40 +21,45 @@ int main(int argc, char** argv)
 
   dictionaries dictOfDicts;
 
-  const std::map< std::string, CommandWrapper > commands = {
-    { "createemptydict", { createemptydict, nullptr } },
-    { "insertword", { insertword, nullptr } },
-    { "insertfile", { insertfile, nullptr } },
-    { "deleteword", { deleteword, nullptr } },
-    { "clear", { clear, nullptr } },
-    { "merge", { merge, nullptr } },
-    { "intersect", { intersect, nullptr } },
-    { "delete", { deleteDict, nullptr } },
+  std::map< std::string, std::function< void(std::istream&, std::ostream&) > > commands;
 
-    { "getfrequency", { nullptr, getfrequency } },
-    { "mostfrequent", { nullptr, mostfrequent } },
-    { "printsorted", { nullptr, printsorted } },
-    { "printbyfrequency", { nullptr, printbyfrequency } },
-    { "size", { nullptr, size } },
-    { "topprelativefrequent", { nullptr, topprelativefrequent } },
-    { "getrelativefrequency", { nullptr, getrelativefrequency } },
-    { "frequencyrangerelative", { nullptr, frequencyrangerelative } },
-    { "bottomprelativefrequent", { nullptr, bottomprelativefrequent } },
-    { "medianfrequency", { nullptr, medianfrequency } }
-  };
+  commands["createemptydict"] = std::bind(createemptydict, std::ref(dictOfDicts), std::placeholders::_1);
+  commands["insertword"] = std::bind(insertword, std::ref(dictOfDicts), std::placeholders::_1);
+  commands["insertfile"] = std::bind(insertfile, std::ref(dictOfDicts), std::placeholders::_1);
+  commands["deleteword"] = std::bind(deleteword, std::ref(dictOfDicts), std::placeholders::_1);
+  commands["clear"] = std::bind(clear, std::ref(dictOfDicts), std::placeholders::_1);
+  commands["merge"] = std::bind(merge, std::ref(dictOfDicts), std::placeholders::_1);
+  commands["intersect"] = std::bind(intersect, std::ref(dictOfDicts), std::placeholders::_1);
+  commands["delete"] = std::bind(deleteDict, std::ref(dictOfDicts), std::placeholders::_1);
+
+  commands["getfrequency"] = std::bind(getfrequency, std::ref(dictOfDicts), std::placeholders::_1, std::ref(std::cout));
+  commands["mostfrequent"] = std::bind(mostfrequent, std::ref(dictOfDicts), std::placeholders::_1, std::ref(std::cout));
+  commands["printsorted"] = std::bind(printsorted, std::ref(dictOfDicts), std::placeholders::_1, std::ref(std::cout));
+  commands["printbyfrequency"] = std::bind(printbyfrequency, std::ref(dictOfDicts), std::placeholders::_1, std::ref(std::cout));
+  commands["size"] = std::bind(size, std::ref(dictOfDicts), std::placeholders::_1, std::ref(std::cout));
+  commands["topprelativefrequent"] = std::bind(topprelativefrequent, std::ref(dictOfDicts), std::placeholders::_1, std::ref(std::cout));
+  commands["getrelativefrequency"] = std::bind(getrelativefrequency, std::ref(dictOfDicts), std::placeholders::_1, std::ref(std::cout));
+  commands["frequencyrangerelative"] = std::bind(frequencyrangerelative, std::ref(dictOfDicts), std::placeholders::_1, std::ref(std::cout));
+  commands["bottomprelativefrequent"] = std::bind(bottomprelativefrequent, std::ref(dictOfDicts), std::placeholders::_1, std::ref(std::cout));
+  commands["medianfrequency"] = std::bind(medianfrequency, std::ref(dictOfDicts), std::placeholders::_1, std::ref(std::cout));
 
   std::string cmd;
   while (std::cin >> cmd)
   {
     try
     {
-      commands.at(cmd)(dictOfDicts, std::cin, std::cout);
+      auto it = commands.find(cmd);
+      if (it == commands.end())
+      {
+        throw std::invalid_argument("<INVALID COMMAND>");
+      }
+      it->second(std::cin, std::cout);
     }
     catch (const std::exception& e)
     {
       std::cerr << e.what() << '\n';
       std::cin.clear();
-      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
   }
 }
