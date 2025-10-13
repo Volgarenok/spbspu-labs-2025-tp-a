7bc0f6b171a967ecd278fa3b313c936aa13ae091
Sand1i1 <igntimofei@gmail.com>
2025-10-13 17:24:51 +0300

    Fix indentation: replace tabs with spaces in data_struct.hpp

--- ignashov.timofei/T2/data_struct.hpp
+++ ignashov.timofei/T2/data_struct.hpp
@@ -7,16 +7,16 @@
 
 namespace ignashov {
 
-	struct DataStruct
-	{
-		unsigned long long key1;
-		std::complex<double> key2;
-		std::string key3;
-	};
+  struct DataStruct
+  {
+    unsigned long long key1;
+    std::complex<double> key2;
+    std::string key3;
+  };
 
-	std::istream& operator>>(std::istream& in, DataStruct& data);
-	std::ostream& operator<<(std::ostream& out, const DataStruct& data);
-	bool operator<(const DataStruct& lhs, const DataStruct& rhs);
+  std::istream& operator>>(std::istream& in, DataStruct& data);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs);
 
 } // namespace ignashov
 
