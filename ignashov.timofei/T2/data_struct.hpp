9776e81f8a7f41c2f59f54ceb700a43483bb5a18
Sand1i1 <igntimofei@gmail.com>
2025-10-13 17:08:08 +0300

    Initial commit for T2

---
+++ ignashov.timofei/T2/data_struct.hpp
@@ -0,0 +1,23 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+#include <iostream>
+#include <complex>
+#include <string>
+
+namespace ignashov {
+
+	struct DataStruct
+	{
+		unsigned long long key1;
+		std::complex<double> key2;
+		std::string key3;
+	};
+
+	std::istream& operator>>(std::istream& in, DataStruct& data);
+	std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+	bool operator<(const DataStruct& lhs, const DataStruct& rhs);
+
+} // namespace ignashov
+
+#endif
