f93ebe3d3754dbcfce014e94750906ea6e20ac6f
Robotophile <ksyunya.iv@gmail.com>
2025-10-27 13:27:58 +0300

    polygon/cpp

--- ivanova.ksenia/T3/polygon.cpp
+++ ivanova.ksenia/T3/polygon.cpp
@@ -24,4 +24,36 @@ namespace ivanova
   {
     return { -x, -y };
   }
+
+  std::size_t Polygon::size() const
+  {
+    return points.size();
+  }
+
+  double Polygon::area() const
+  {
+    if (points.size() < 3)
+    {
+      return 0.0;
+    }
+    double sum = std::inner_product(
+      points.begin(), points.end() - 1,
+      points.begin() + 1,
+      0.0,
+      std::plus<>(),
+      crossMultiply
+    );
+    sum += crossMultiply(points.back(), points.front());
+    return std::abs(sum) * 0.5;
+  }
+
+  bool Polygon::operator==(const Polygon& other) const
+  {
+    return points == other.points;
+  }
+
+  double Polygon::crossMultiply(const Point& a, const Point& b)
+  {
+    return a.x * b.y - a.y * b.x;
+  }
 }
